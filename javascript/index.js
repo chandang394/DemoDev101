0004c6ed44083da090c422b411bd1641216dc283
chandan <chandangarg394@gmail.com>
2022-02-10 22:46:33 +0530

    js started

---
+++ javascript/index.js
@@ -0,0 +1,29 @@
+// starting javascript 
+// var,let,const
+
+
+// variable declearation
+
+
+ var a; 
+// it may be anything
+
+
+a=10;
+console.log(a);
+
+a="hello";
+console.log(a);
+// typeof operator
+ console.log(typeof a);
+
+ a=true;
+ console.log(a);
+ console.log(typeof a);
+
+ a=null;
+ console.log(a);
+ console.log(typeof a);
+
+ a=256;
+ console.log(a);
